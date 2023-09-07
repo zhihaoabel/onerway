f9977ff96367b4c282649e78269088f7baafd970
Abel <aa1650694369@gmail.com>
2023-09-07 18:22:10 +0800

    init

---
+++ src/main/java/com/abel/onerway/OnerwayApplication.java
@@ -0,0 +1,13 @@
+package com.abel.onerway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnerwayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnerwayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abel/onerway/OnerwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abel.onerway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnerwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
